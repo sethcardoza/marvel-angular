ef46e4cc053d0b8f40e1c3721ab5f112abba9fb5
Seth Cardoza <sethcardoza@Seths-MacBook-Pro.local>
2019-09-12 21:34:27 -0400

    adding component shells

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { ComicsComponent } from './components/comics/comics.component';
+import { CreatorsComponent } from './components/creators/creators.component';
+import { EventsComponent } from './components/events/events.component';
+import { SeriesComponent } from './components/series/series.component';
+import { StoriesComponent } from './components/stories/stories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    ComicsComponent,
+    CreatorsComponent,
+    EventsComponent,
+    SeriesComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule,
